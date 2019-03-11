869a0be1011816584913cd89d32b3a701f6927d7
WalaaMedhat <nicedree@gmail.com>
2019-03-11 06:09:57 +0200

    fixed lighting,camera and display the part that the user clicked on

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ const express = require('express');
 const app = express();
 
 
+app.use(function(req, res, next) {
+  // check  extension
+  console.log(req.originalUrl,'url');
+  next();
+}, express.static(__dirname));
 app.use(express.static(path.join(__dirname, '../public')));
 app.use('/*', express.static(path.join(__dirname,'..','public','index.html')));
 
